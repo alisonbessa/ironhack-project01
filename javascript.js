b291886fa8624fa3675c7d9a2415845f8d1c02ba
Alison <alisonbessa@gmail.com>
2019-12-09 14:51:39 -0300

    Add first falling obstacle

--- javascript.js
+++ javascript.js
@@ -1,33 +1,26 @@
-let myGamePiece;
-let myObstacles = [];
-let velocity = 6;
-
-
-var myGameArea = {
+let myGameArea = {
     canvas: document.createElement("canvas"),
-  frames: 0,
-  start: function() {
-    this.canvas.width = 400;
-    this.canvas.height = 700;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 30);
-    window.addEventListener('keydown', function (e) {
-      myGameArea.keys = (myGameArea.keys || []);
-      myGameArea.keys[e.keyCode] = true;
-    })
-    window.addEventListener('keyup', function (e) {
-      myGameArea.keys[e.keyCode] = false;
-    })
-  },
-  clear: function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
-  stop: function() {
-    clearInterval(this.interval);
-  },
-
-
+    frames: 0,
+    start: function() {
+        this.canvas.width = 400;
+        this.canvas.height = 700;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 30);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = false;
+        })
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function() {
+        clearInterval(this.interval);
+    },
 };
 
 
@@ -54,21 +47,38 @@ class Component {
     }
 }
 
-var player = new Component(30, 30, "red", 185, 600);
+let velocity = 10;
+let obstacleSpeed = 15;
+let player = new Component(30, 30, "red", 185, 600);
+let obstacle = new Component(30, 30, "yellow", Math.random()*400, -100);
 
 function updateGameArea() {
     myGameArea.clear();
+    playerUpdate();
+    playerMovement()
+    obstacles();
+    
+}
+
+function obstacles(){
+    obstacle.update();
+    obstacle.y += obstacleSpeed;
+}
+
+function playerUpdate(){
     player.newPos();
     player.update();
+}
+
+function playerMovement(){
     player.speedX = 0;
     player.speedY = 0;
     if (myGameArea.keys && myGameArea.keys[37]) {player.speedX = -velocity; }
     if (myGameArea.keys && myGameArea.keys[39]) {player.speedX = velocity; }
     if (myGameArea.keys && myGameArea.keys[38]) {player.speedY = -velocity; }
     if (myGameArea.keys && myGameArea.keys[40]) {player.speedY = velocity; }
-  
-    
 }
+
 
 myGameArea.start();
 
